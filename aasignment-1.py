dc9e88a8de2381d51a7d31b8fe6acc8945ab85fd
TusharKulkarni04253N <kulkarnit498@gmail.com>
2025-04-20 15:14:12 +0530

    Add files via upload

---
+++ aasignment-1.py
@@ -0,0 +1,18 @@
+# assignment 1
+# problem 1
+a = input("insert one digit 1 ")
+b = input("insert one digit 2 ")
+a = int(a)
+b = int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+# problem 2
+
+Name = input("Enter your name ")
+Surname = input("Enter your surname ")
+
+print("Hello,"+ (Name) +(Surname)+"! welcome to the python program")
